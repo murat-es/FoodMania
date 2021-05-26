badbece6ed4426f6e141d415fb8019c229c5b884
murateist <murateist@users.noreply.github.com>
2021-05-26 09:18:43 +0300

    updated some features

--- app/src/main/java/com/murates/durumcumuratusta/MainActivity.java
+++ app/src/main/java/com/murates/durumcumuratusta/MainActivity.java
@@ -13,7 +13,6 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
